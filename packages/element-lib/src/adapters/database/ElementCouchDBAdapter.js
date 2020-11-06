98010ea7932a5023710e577c2d758bafcd695255
Shimmy Mallonga <shimmy.mallonga@mattr.global>
2020-11-06 15:12:10 +1300

    feat(element-lib): coucdb update remote

--- packages/element-lib/src/adapters/database/ElementCouchDBAdapter.js
+++ packages/element-lib/src/adapters/database/ElementCouchDBAdapter.js
@@ -31,7 +31,7 @@ class ElementCouchDBAdapter {
 
   async init() {
     if (!this.created) {
-      await this.couch.createDatabase(this.name).catch(err => {
+      await this.couch.createDatabase(this.name).catch((err) => {
         // Sometimes the db already exists
         if (err.body.error !== 'file_exists') {
           throw err;
