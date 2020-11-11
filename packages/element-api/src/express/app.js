a2ba6ecbc2ff759d8e90e86bcf2656fcef874ad0
Shimmy Mallonga <shimmy.mallonga@mattr.global>
2020-11-12 12:08:26 +1300

    feat(elkement-api): allow cors

--- packages/element-api/src/express/app.js
+++ packages/element-api/src/express/app.js
@@ -13,6 +13,11 @@ const app = express();
 const onErrorResponse = require('../../src/express/onErrorResponse');
 
 // Automatically allow cross-origin requests
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(cors({ origin: true }));
 app.use(express.json());
 if (process.env.NODE_ENV === 'test') {
